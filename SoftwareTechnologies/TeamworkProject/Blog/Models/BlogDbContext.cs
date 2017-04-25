174afa6a3ce9b98a139c84614a0c8ad7ed9d5841
Iliyan7 <iliann52@gmail.com>
2017-04-25 17:52:43 +0300

    Blog Created with MVC Pattern by VS and a style re-designed

---
+++ SoftwareTechnologies/TeamworkProject/Blog/Models/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Blog.Models
+{
+    public class BlogDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BlogDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BlogDbContext Create()
+        {
+            return new BlogDbContext();
+        }
+    }
+}
